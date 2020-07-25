62f88a72b41f5f213fe02ed98d63992fc819586e
Mingsicogi <codedoctor119@gmail.com>
2020-07-25 15:48:24 +0900

    + add custom session info using custom filter
    Signed-off-by: Mingsicogi <codedoctor119@gmail.com>

--- src/main/java/mins/study/session/SessionAdditionalInfo.java
+++ src/main/java/mins/study/session/SessionAdditionalInfo.java
@@ -1,27 +1,45 @@
 package mins.study.session;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import ua_parser.Client;
 import ua_parser.Parser;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.Serializable;
 
-@Getter
-public class SessionAdditionalInfo {
-    private final String ip;
-    private final String device;
-    private final String agent;
-    private final String os;
+@Slf4j
+@Getter @Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class SessionAdditionalInfo implements Serializable {
+    private static final long serialVersionUID = -4142386168596393316L;
 
-    public SessionAdditionalInfo(HttpServletRequest request) throws Exception{
+    private String ip;
+    private String device;
+    private String agent;
+    private String os;
+
+    public SessionAdditionalInfo(HttpServletRequest request) {
         this.ip = request.getRemoteAddr();
 
         String header = request.getHeader("User-Agent");
-        Parser parser = new Parser();
-        Client userAgent = parser.parse(header);
+        try {
+            Parser parser = new Parser();
+            Client userAgent = parser.parse(header);
 
-        this.agent = userAgent.userAgent.family;
-        this.device = userAgent.device.family;
-        this.os = userAgent.os.family;
+            this.agent = userAgent.userAgent.family;
+            this.device = userAgent.device.family;
+            this.os = userAgent.os.family;
+        } catch (IOException e) {
+            log.error("UAParser is not working... Device info will be set by default...");
+            this.device = "unknown";
+            this.agent = "unknown";
+            this.os = "unknown";
+        }
     }
 }
--- src/main/java/mins/study/session/Welcome.java
+++ src/main/java/mins/study/session/Welcome.java
@@ -1,5 +1,6 @@
 package mins.study.session;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContext;
@@ -8,10 +9,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.session.FindByIndexNameSessionRepository;
 import org.springframework.session.Session;
 import org.springframework.session.data.redis.RedisIndexedSessionRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,6 +23,7 @@ import java.util.Map;
 public class Welcome {
 
     private final RedisIndexedSessionRepository redisIndexedSessionRepository;
+    private final ObjectMapper objectMapper;
 
     @RequestMapping("/welcome")
     public String welcome(Model model) {
@@ -37,6 +39,15 @@ public class Welcome {
     public ResponseEntity<Object> getSessionInfo(String username) {
         Map<String, ? extends Session> byPrincipalName = redisIndexedSessionRepository.findByPrincipalName(username);
         return ResponseEntity.ok(byPrincipalName);
+    }
+
+    @GetMapping("/session/info/details")
+    @ResponseBody
+    public ResponseEntity<Object> getSessionInfoDetails(String username, HttpServletRequest request) {
+        Map<String, ? extends Session> sessionInfo = redisIndexedSessionRepository.findByIndexNameAndIndexValue(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, username);
+        Session session = sessionInfo.get(request.getSession().getId());
+        SessionAdditionalInfo addInfo = objectMapper.convertValue(session.getAttribute("addInfo"), SessionAdditionalInfo.class);
+        return ResponseEntity.ok(addInfo);
     }
 
     @GetMapping("/session/make")
