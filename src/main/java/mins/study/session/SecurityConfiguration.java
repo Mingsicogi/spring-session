fd9ed0e2bc58d4927a94a114c759c7d7a046d48f
Mingsicogi <codedoctor119@gmail.com>
2020-07-25 14:24:55 +0900

    + security configuration

--- src/main/java/mins/study/session/SecurityConfiguration.java
+++ src/main/java/mins/study/session/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -28,7 +27,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/session/**").permitAll();
+
         http.formLogin().defaultSuccessUrl("/welcome");
+
+        http.authorizeRequests().anyRequest().authenticated(); // 나머지 페이지는 로그인 후 이용 가능
     }
 
     @Bean
